b9816564bcc49362a603d05461001103fc7a36e2
Kathleen Howard <kmaryhoward@gmail.com>
2019-01-21 23:11:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,3 +5,12 @@ def countdown(number)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  sleep 5
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
